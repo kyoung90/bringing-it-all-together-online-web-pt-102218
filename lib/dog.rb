72e23b0aa45e5d1315b49e3ea28d51cf514c3706
Kenneth Young Castro <kenneth.young90@gmail.com>
2019-01-20 16:20:23 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -29,7 +29,6 @@ class Dog
     else
       DB[:conn].execute("INSERT INTO dogs (name, breed) VALUES (?, ?)", self.name, self.breed)
       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
-      self
     end
   end
 
